bb5b899823f08f53a89ea641bdbb790f72766ae3
RAFAEL CAVALCANTE <00001103579691sp@aluno.educacao.sp.gov.br>
2023-09-04 19:11:22 -0300

    setima parte

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 function mostraSenha(){
-    var inputPass = document.getElementById('senha')
-    var btnShowPass = ducument.getElementById('btn-senha')
+    var inputPass = document.getElementById('senha');
+    var btnShowPass = document.getElementById('btn-senha');
 
     if(inputPass.type === 'password'){
-        inputPass.setAttribute('type', 'text')
-        btnShowPass.classList.replace('bi-eye-fill','bi-eye-slash-fill')
+        inputPass.setAttribute('type', 'text');
+        btnShowPass.classList.replace('bi-eye-fill', 'bi-eye-slash-fill');
     }else{
-        inputPass.setAttribute('type', 'password')
-        btnShowPass.classList.replace('bi-eye-slash-fill','bi-eye-fill')
+        inputPass.setAttribute('type', 'password');
+        btnShowPass.classList.replace('bi-eye-slash-fill', 'bi-eye-fill');
     }
 }
